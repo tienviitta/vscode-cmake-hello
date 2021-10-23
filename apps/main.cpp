a954e9ef46965727904c850a54cd0c23b8003a19
Väisänen, Petri <petri.j.vaisanen@gmail.com>
2021-10-23 15:45:49 +0300

    Second part, shared lib, added

--- apps/main.cpp
+++ apps/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int, char **)
 {
-    std::string helloJim = generateHelloString("Jim");
+    std::string helloJim = generateHelloString("Jim II");
     std::cout << helloJim << std::endl;
 
     return 0;
