0848b513ce7903e7f65da809de89c822ed8980cb
Väisänen, Petri <petri.j.vaisanen@gmail.com>
2021-10-23 15:13:33 +0300

    CMake VSCode Tutorial

---
+++ apps/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "hello.h"
+
+int main(int, char **)
+{
+    std::string helloJim = generateHelloString("Jim");
+    std::cout << helloJim << std::endl;
+
+    return 0;
+}
